db48b39df1e68977d2f5b91da807d3f72445eec7
Aulbourn <aulbourn@gmail.com>
2020-06-24 01:38:06 -0400

    fixed rubocop lints

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
-  validates :full_name, presence: true;
+  validates :full_name, presence: true
   validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[\w+\-.]*\.[a-z]+\z/i }, presence: true, uniqueness: true
   validates :password, length: { in: 6..16 }, presence: true
